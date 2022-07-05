157e820b6d5cea177ad0b72ce1687ed9e049c114
Cole Blackman <coleblax@gmail.com>
2022-07-05 13:08:34 -0400

    Added macro config scan

--- placement_cfg_gen.py
+++ placement_cfg_gen.py
@@ -4,6 +4,7 @@ import sys
 import os
 import re
 
+
 if len(sys.argv) > 1:
     top_path = sys.argv[1]
     print("\n\033[1;32mProject\033[m location supplied as a script argument: \033[1;32m", top_path, "\033[m")
@@ -12,7 +13,7 @@ else:
     if top_path == "default" or top_path == "" or top_path == "local":
         top_path = '.'
 # Set paths for config.tcl, macro_placement.cfg
-config_tcl_path = top_path + "config.tcl"
+config_tcl_path = top_path + "/config.tcl"
 macro_placement_cfg_path = top_path + "macro_placement.cfg"
 if(os.path.exists(macro_placement_cfg_path)):
     rem = input('\nExisting macro_placement.cfg found on project path. Do you want to overwrite it? Type yes or no\n > ')
@@ -29,8 +30,11 @@ valid_fpga_core_found = False
 use_fpga_top = False
 use_fpga_core = False
 
-fpga_top_path = top_path + "fpga_top.v"
-fpga_core_path = top_path + "fpga_core.v"
+fpga_top_path = top_path + "/fpga_top.v"
+fpga_core_path = top_path + "/fpga_core.v"
+
+print("Searching for fpga_top.v at ", fpga_top_path)
+print("Searching for fpga_core.v at ", fpga_core_path)
 
 if(os.path.exists(fpga_top_path)):
     valid_fpga_top_found = True
@@ -73,19 +77,48 @@ print("grid_number: ", grid_number)
 # TODO Ensure there is a .gds for each macro
 
 print("Searching for necessary macros in ", top_path, '/gds...')
+macro_tcl_path = top_path+"/macro_tcls"
+foundMacros = os.walk(macro_tcl_path).__next__()[2]
+numFoundMacros = len(foundMacros)
 
+print("Found", numFoundMacros, "macro[s].")
+print(foundMacros)
 
-# TODO Source every single macro's config.tcl and get the sizes
+# Make a parallel array with just the paths of the macro modules
+macroPaths = []
+# Make a parallel array with just the names of the modules
+macroNames = []
+for macro in foundMacros:
+    macroPaths.append((macro_tcl_path+"/"+macro))
+    macroNames.append(macro[0:-4])
+print(macroNames)
+print(macroPaths)
 
-# Read in overall config.tcl
-with open(config_tcl_path, 'r', encoding="utf-8") as config_tcl:
-    config_tcl_data = config_tcl.read()
-
+    
+# Ensure there is a valid overall config.tcl
+print("Searching for valid config.tcl at ", config_tcl_path)
+if(os.path.exists(config_tcl_path)):
+    # Read in overall config.tcl
+    print("Valid config.tcl found.")
+    with open(config_tcl_path, 'r', encoding="utf-8") as config_tcl:
+        config_tcl_data = config_tcl.read()
+else:
+    print("No valid config.tcl found. Ensure it is in the appropriate directory and is valid. \nAborting...")
+    sys.exit()
+    
 # Parse config.tcl and set various config variables
 # Here is the regex: (?<=set \:\:env\(DIE_AREA\) ).*
 # Take a look here for inspiration https://stackoverflow.com/questions/4248010/how-to-exclude-comment-lines-when-searching-with-regular-expression
-
+# FIXME does not return any output currently
+ConfigTCLRegularExpression = re.compile("(?<=set \:\:env\(DIE_AREA\) ).*", re.MULTILINE|re.DOTALL)
+for search in ConfigTCLRegularExpression.findall(config_tcl_data):
+    print(search)
 # config variables = Die area, macro areas, etc
+# There are 4 types: grid_clb, sb_x_y, cbx_x_y, cby_x_y
+# First, grid_clb:
+grid_clb_path = top_path + "fpga_core/grid_clb/config.tcl"
+# TODO Now read in that file and parse the width and height of the macro
+
 horizontal_size = 6000
 
 vertical_size = 6000
@@ -99,8 +132,7 @@ block_height = (500, 200, 600)
 # go into macro_placement.cfg
 macro_placement = ""
 
-### Generate the grid
-
+# Generate the grid
 
 # Find smallest permissible gap between two centers
 # This works because half_width*2 = block_width
@@ -110,8 +142,11 @@ min_dist = max(min_width_dist, min_height_dist)
 
 # Calculate how many we can fit, 
 # both horizontally and vertically
-horizontal_number = (horizontal_size-(min_padding*2))/(min_width_dist+min_gap)
-vertical_number = (vertical_size-(min_padding*2))/(min_height_dist+min_gap)
+print("\n---------------------------\n")
+horizontal_number = (horizontal_size-(min_padding*2))//(min_width_dist+min_gap)
+print("horizontal_number: ", horizontal_number)
+vertical_number = (vertical_size-(min_padding*2))//(min_height_dist+min_gap)
+print("vertical number: ", vertical_number)
 # Put all x and y coords for centers into tuples
 
 centers_x = [min_gap]
@@ -130,10 +165,9 @@ for i in range(vertical_number-1):
 
 
 
-
 # grid_clib = every other, except it can never be on an edge
 
-# Output to macro_placement.cfg, neatly configured
+# Output to macro_placement.cfg, configured
 # with the correct inst name from the gds file
 
 macro_placement_path = top_path + "macro_placement.cfg"
